f46fbb5f8c1f3f48c8069ea2baa197d7aea3dbaa
xosedelara <97544706+xosedelara@users.noreply.github.com>
2022-01-25 13:11:33 -0600

    https://github.com/xosedelara/bootcamp/tree/main/projects/01-cipher

--- projects/01-cipher/src/index.js
+++ projects/01-cipher/src/index.js
@@ -1,3 +1,18 @@
+/* eslint-disable no-console */
 import cipher from './cipher.js';
+const returnInfo = document.getElementById("returnMsg");
+document.getElementById("encodeBtn").addEventListener("click", function(){
+    const textToCipher = (document.getElementById("inText").value);
+    const offset = document.getElementById("clueOne").value;
+    const returnCiphMsg= document.getElementById("returnText");
+    returnInfo.innerHTML="Tu mensaje cifrado es:";
+    returnCiphMsg.innerHTML = cipher.encode(offset, textToCipher);
+})
+document.getElementById("decodeBtn").addEventListener("click", function(){
+    const textToDecipher = (document.getElementById("inText").value);
+    const offset = document.getElementById("clueOne").value;
+    const returnDMsg= document.getElementById("returnText");
+    returnInfo.innerHTML="Esto decía el mensaje original:"
+    returnDMsg.innerHTML = cipher.decode(offset, textToDecipher);
 
-console.log(cipher);
+})
