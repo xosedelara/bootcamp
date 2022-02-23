b5d0cf87df589d330e37b6376bb3c0a22330f2f0
xosedelara <97544706+xosedelara@users.noreply.github.com>
2022-02-22 18:14:18 -0600

    tests

--- projects/01-cipher/src/cipher.js
+++ projects/01-cipher/src/cipher.js
@@ -1,96 +1,64 @@
 /* eslint-disable no-console */
 const cipher = {
-  encode: (offset, textToCipher) =>{
-    let cipheredMsg='';
-    if (textToCipher===null || textToCipher=== ""){
-    throw TypeError("Ingresa por favor un texto a cifrar");
-    }else{
-      if (offset === null || offset=== 0){
-        throw TypeError("Ingresa por favor una clave");
-      }else{
-        for (let a=0;a<textToCipher.length;a++){
-          let asciiNum = textToCipher.charCodeAt(a);
-          if (asciiNum >= 65 && asciiNum <= 90){
-            let cipheredAscii = ((asciiNum-65 + parseInt(offset))%26 + 65);
-            let cipheredLetter = String.fromCharCode(cipheredAscii);
-            cipheredMsg+=cipheredLetter;
-          }else{
-            if (asciiNum>=48 && asciiNum <= 57){
-             cipheredMsg += String.fromCharCode((asciiNum-48 + parseInt(offset))%10 + 48);
-            }else{
-              if(asciiNum>=34 && asciiNum <= 47){
-                cipheredMsg += String.fromCharCode((asciiNum-34 + parseInt(offset))%14 + 34);
-              }else{
-               if(asciiNum>=91 && asciiNum<=96){
-                  cipheredMsg += String.fromCharCode((asciiNum-91 + parseInt(offset))%6 + 91);
-                }else{
-                  if(asciiNum>=123 && asciiNum<=126){
-                    cipheredMsg += String.fromCharCode((asciiNum-123 + parseInt(offset))%4 + 123);
-                  }else{
-                    if(asciiNum>=97 && asciiNum<=122){
-                      cipheredMsg += String.fromCharCode((asciiNum-97 + parseInt(offset))%26 + 97);
-                    }else{
-                      if (asciiNum>=58 && asciiNum<=63){
-                        cipheredMsg += String.fromCharCode((asciiNum-58 + parseInt(offset))%6 + 58);
-                      }else{
-                        cipheredMsg = cipheredMsg + textToCipher[a];
-                      }
-                    }
-                  }
-                }
-              } 
-            }
-          } 
+  encode: (offset, textToCipher) => {
+    let cipheredMsg = '';
+    if (offset === null || offset === 0) {
+      throw TypeError("Ingresa por favor una clave");
+    } else {
+      for (let a = 0; a < textToCipher.length; a++) {
+        let asciiNum = textToCipher.charCodeAt(a);
+        if (asciiNum >= 65 && asciiNum <= 90) {
+          let cipheredAscii = ((asciiNum - 65 + parseInt(offset)) % 26 + 65);
+          let cipheredLetter = String.fromCharCode(cipheredAscii);
+          cipheredMsg += cipheredLetter;
+        } else if (asciiNum >= 48 && asciiNum <= 57) {
+          cipheredMsg += String.fromCharCode((asciiNum - 48 + parseInt(offset)) % 10 + 48);
+        } else if (asciiNum >= 34 && asciiNum <= 47) {
+          cipheredMsg += String.fromCharCode((asciiNum - 34 + parseInt(offset)) % 14 + 34);
+        } else if (asciiNum >= 91 && asciiNum <= 96) {
+          cipheredMsg += String.fromCharCode((asciiNum - 91 + parseInt(offset)) % 6 + 91);
+        } else if (asciiNum >= 123 && asciiNum <= 126) {
+          cipheredMsg += String.fromCharCode((asciiNum - 123 + parseInt(offset)) % 4 + 123);
+        } else if (asciiNum >= 97 && asciiNum <= 122) {
+          cipheredMsg += String.fromCharCode((asciiNum - 97 + parseInt(offset)) % 26 + 97);
+        } else if (asciiNum >= 58 && asciiNum <= 63) {
+          cipheredMsg += String.fromCharCode((asciiNum - 58 + parseInt(offset)) % 6 + 58);
+        } else {
+          cipheredMsg = cipheredMsg + textToCipher[a];
+        }
         console.log(cipheredMsg)
-        }return cipheredMsg;
-      }
+      } return cipheredMsg;
     }
   },
-  decode: (offset, textToDecipher) =>{
-    let decipheredMsg='';
-    if (textToDecipher.value===null || textToDecipher=== ""){
-      throw TypeError("Ingresa por favor un texto a decifrar");
-      }else{
-        if (offset.value === null || textToDecipher=== 0){
-          throw TypeError("Ingresa por favor la clave");
-        }else{
-          for (let a=0;a<textToDecipher.length;a++){
-            let asciiNum = textToDecipher.charCodeAt(a);
-            if (asciiNum >= 65 && asciiNum <= 90){
-              let decipheredAscii = ((asciiNum-90 - parseInt(offset))%26 + 90);
-              let decipheredLetter = String.fromCharCode(decipheredAscii);
-              decipheredMsg+=decipheredLetter;
-            }else{
-              if (asciiNum>=48 && asciiNum <= 57){
-                decipheredMsg += String.fromCharCode((asciiNum-57 - parseInt(offset))%10 + 57);
-              }else{
-                if(asciiNum>=34 && asciiNum <= 47){
-                  decipheredMsg += String.fromCharCode((asciiNum-47 - parseInt(offset))%14 + 47);
-                }else{
-                  if(asciiNum>=91 && asciiNum<=96){
-                    decipheredMsg += String.fromCharCode((asciiNum-9691 - parseInt(offset))%6 + 91);
-                  }else{
-                    if(asciiNum>=123 && asciiNum<=126){
-                      decipheredMsg += String.fromCharCode((asciiNum-126 - parseInt(offset))%4 + 126);
-                    }else{
-                      if(asciiNum>=97 && asciiNum<=122){
-                        decipheredMsg += String.fromCharCode((asciiNum-122 - parseInt(offset))%26 + 122);
-                      }else{
-                        if (asciiNum>=58 && asciiNum<=63){
-                          decipheredMsg += String.fromCharCode((asciiNum-63 - parseInt(offset))%6 + 63);
-                        }else{
-                          decipheredMsg = decipheredMsg + textToDecipher[a];
-                        }
-                      }
-                    }
-                  }
-                } 
-              } 
-            } 
-          console.log(decipheredMsg)
-          }return decipheredMsg;
+  decode: (offset, textToDecipher) => {
+    let decipheredMsg = '';
+    if (offset.value === null || textToDecipher === 0) {
+      throw TypeError("Ingresa por favor la clave");
+    } else {
+      for (let a = 0; a < textToDecipher.length; a++) {
+        let asciiNum = textToDecipher.charCodeAt(a);
+        if (asciiNum >= 65 && asciiNum <= 90) {
+          let decipheredAscii = ((asciiNum - 90 - parseInt(offset)) % 26 + 90);
+          let decipheredLetter = String.fromCharCode(decipheredAscii);
+          decipheredMsg += decipheredLetter;
+        } else if (asciiNum >= 48 && asciiNum <= 57) {
+          decipheredMsg += String.fromCharCode((asciiNum - 57 - parseInt(offset)) % 10 + 57);
+        } else if (asciiNum >= 34 && asciiNum <= 47) {
+          decipheredMsg += String.fromCharCode((asciiNum - 47 - parseInt(offset)) % 14 + 47);
+        } else if (asciiNum >= 91 && asciiNum <= 96) {
+          decipheredMsg += String.fromCharCode((asciiNum - 96 - parseInt(offset)) % 6 + 96);
+        } else if (asciiNum >= 123 && asciiNum <= 126) {
+          decipheredMsg += String.fromCharCode((asciiNum - 126 - parseInt(offset)) % 4 + 126);
+        } else if (asciiNum >= 97 && asciiNum <= 122) {
+          decipheredMsg += String.fromCharCode((asciiNum - 122 - parseInt(offset)) % 26 + 122);
+        } else if (asciiNum >= 58 && asciiNum <= 63) {
+          decipheredMsg += String.fromCharCode((asciiNum - 63 - parseInt(offset)) % 6 + 63);
+        } else {
+          decipheredMsg = decipheredMsg + textToDecipher[a];
         }
-      }
+        console.log(decipheredMsg)
+      } return decipheredMsg;
+    }
   }
 }
 export default cipher
--- projects/01-cipher/test/cipher.spec.js
+++ projects/01-cipher/test/cipher.spec.js
@@ -28,34 +28,27 @@ describe('cipher', () => {
     it('should return "HIJKLMNOPQRSTUVWXYZABCDEFG" for "ABCDEFGHIJKLMNOPQRSTUVWXYZ" with offset 33', () => {
       expect(cipher.encode(33, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ')).toBe('HIJKLMNOPQRSTUVWXYZABCDEFG');
     });
-
-    // Hacker edition
-    //
-    // [Español]
-    // Si decides agregar soporte para minúsculas descomenta el test a
-    // continuación.
-    //
-    // [Português]
-    // Se quiser adicionar testes para letras minúsculas, descomente o teste
-    // abaixo.
-    //
     it('should return "hijklmnopqrstuvwxyzabcdefg" for "abcdefghijklmnopqrstuvwxyz" with offset 33', () => {
       expect(cipher.encode(33, 'abcdefghijklmnopqrstuvwxyz')).toBe('hijklmnopqrstuvwxyzabcdefg');
     });
-
-    // Hacker edition
-    //
-    // [Español]
-    // Si decides implementar soporte para caracteres no alfabéticos descomenta
-    // el test a continuación.
-    //
-    // [Português]
-    // Se quiser adicionar testes para caracteres não alfabéticos, descomente o
-    // teste abaixo.
-    //
     it('should return " !@" for " !@"', () => {
       expect(cipher.encode(33, ' !@')).toBe(' !@');
     });
+    it('should return "234" for "012"', () => {
+      expect(cipher.encode(2, '012')).toBe('234');
+    });
+    it('should return "$%&" for ""#$"', () => {
+      expect(cipher.encode(2, '"#$')).toBe('$%&');
+    });
+    it('should return "`" for "^"', () => {
+      expect(cipher.encode(2, '^')).toBe('`');
+    });
+    it('should return "}" for "{"', () => {
+      expect(cipher.encode(2, '{')).toBe('}');
+    });
+    it('should return "<" for ":"', () => {
+      expect(cipher.encode(2, ':')).toBe('<');
+    }); 
   });
 
   describe('cipher.decode', () => {
@@ -75,34 +68,28 @@ describe('cipher', () => {
       expect(cipher.decode(33, 'HIJKLMNOPQRSTUVWXYZABCDEFG')).toBe('ABCDEFGHIJKLMNOPQRSTUVWXYZ');
     });
 
-    //
-    // Hacker edition
-    //
-    // [Español]
-    // Si decides agregar soporte para minúsculas descomenta el test a
-    // continuación.
-    //
-    // [Português]
-    // Se quiser adicionar testes para letras minúsculas, descomente o teste
-    // abaixo.
-    //
     it('should return "abcdefghijklmnopqrstuvwxyz" for "hijklmnopqrstuvwxyzabcdefg" with offset 33', () => {
       expect(cipher.decode(33, 'hijklmnopqrstuvwxyzabcdefg')).toBe('abcdefghijklmnopqrstuvwxyz');
     });
 
-    // Hacker edition
-    //
-    // [Español]
-    // Si decides implementar soporte para caracteres no alfabéticos descomenta
-    // el test a continuación.
-    //
-    // [Português]
-    // Se quiser adicionar testes para caracteres não alfabéticos, descomente o
-    // teste abaixo.
-    //
     it('should return " !@" para " !@"', () => {
       expect(cipher.decode(33, ' !@')).toBe(' !@');
     });
+    it('should return "012" for "234"', () => {
+      expect(cipher.decode(2, '234')).toBe('012');
+    });
+    it('should return ""#$" for "$%&"', () => {
+      expect(cipher.decode(2, '$%&')).toBe('"#$');
+    });
+    it('should return "^" for "`"', () => {
+      expect(cipher.decode(2, '`')).toBe('^');
+    });
+    it('should return "{" for "}"', () => {
+      expect(cipher.decode(2, '}')).toBe('{');
+    });
+    it('should return ":" for "<"', () => {
+      expect(cipher.decode(2, '<')).toBe(':');
+    }); 
   });
 
 });
